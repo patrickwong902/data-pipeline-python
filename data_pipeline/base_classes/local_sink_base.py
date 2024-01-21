5aec62f67d5664f8c6bab96e5f9012b72a7eb750
patrickwong902 <42110711+patrickwong902@users.noreply.github.com>
2024-01-21 17:08:04 +0100

    added local data stratgies

---
+++ data_pipeline/base_classes/local_sink_base.py
@@ -0,0 +1,28 @@
+from abc import ABC
+from data_pipeline.base_classes.data_structures.sink import SinkConfig
+
+
+class LocalStrategyBase(ABC):
+    def __init__(self, dataframe, sink_config: SinkConfig):
+        super().__init__()
+        self.mode = sink_config.mode
+        self.storage_account = sink_config.storage_account
+        self.container_name = sink_config.container_name
+        self.dataframe = dataframe
+        self.full_path = None
+
+    def create_full_path(self, path):
+        self.full_path = "/".join([
+            self.storage_account,
+            self.container_name,
+            path
+        ])
+
+    def write(self, path, file_format):
+        print("Writing to (local) Data Lake started")
+        self.create_full_path(path)
+        print(f"Writing to {self.full_path}")
+        self.dataframe.write.format(file_format).option("overWriteSchema", "true").mode(self.mode).save(
+            path=self.full_path
+        )
+        print("Writing to (local) Data Lake is done!")
---
+++ data_pipeline/base_classes/local_source_base.py
@@ -0,0 +1,27 @@
+from abc import ABC
+from data_pipeline.base_classes.data_structures.source import SourceConfig
+
+
+class SourceLocalBase(ABC):
+    def __init__(self, source_config: SourceConfig):
+        super().__init__()
+        self.storage_account = source_config.storage_account
+        self.container_name = source_config.container_name
+        self.dataframe = None
+        self.full_path = None
+
+    def create_full_path(self, path):
+        self.full_path = "/".join([
+            self.storage_account,
+            self.container_name,
+            path
+        ])
+
+    def read(self, spark, path, file_format):
+        print("Reading from (local) Data Lake started")
+        self.create_full_path(path)
+        print(f"Reading from {self.full_path}")
+        self.dataframe = spark.read.format(file_format).load(self.full_path)
+        print(f"Reading from (local) Data Lake is done")
+
+
